a18719c0fd65db0f21e79df14e302af28d696308
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-01 19:50:22 +0200

    Add `create_sphere` method to `Primitives` class
    Introduced a new method `create_sphere` for generating sphere geometry, which includes specifying segments and radius. This method computes the vertices and faces required to represent a sphere in 3D space and leverages mathematical functions for its calculations.

--- code/core/primitives.cpp
+++ code/core/primitives.cpp
@@ -1,4 +1,5 @@
 #include "primitives.hpp"
+#include "math/functions.hpp"
 
 namespace editor
 {
@@ -70,5 +71,53 @@ namespace editor
                 { 20, 21, 22 }, { 22, 23, 20 }  //  down face
             }
         };
+    }
+
+    geometry Primitives::create_sphere(const uint32_t segments, const float radius)
+    {
+        geometry sphere;
+
+        const float hr    = radius / 2.0f;
+        const float stack = engine::math::pi() / static_cast<float>(segments);
+        const float slice = engine::math::pi() / static_cast<float>(segments) * 2.0f;
+
+        for (uint32_t i = 0; i <= segments; ++i)
+        {
+            const float phi = stack * static_cast<float>(i);
+
+            const float sin_phi = hr * engine::math::sin(phi);
+            const float cos_phi = hr * engine::math::cos(phi);
+
+            for (uint32_t j = 0; j <= segments; ++j)
+            {
+                const float theta = static_cast<float>(j) * slice;
+
+                const engine::vec3 position
+                {
+                    sin_phi * engine::math::cos(theta), cos_phi,
+                    sin_phi * engine::math::sin(theta),
+                };
+
+                sphere.vertices.emplace_back(position, position.normalized());
+            }
+        }
+
+        for (uint32_t i = 0; i < segments; ++i)
+        {
+            for (uint32_t j = 0; j <  segments; ++j)
+            {
+                const uint32_t next = segments + 1;
+
+                const uint32_t top_left     = i * next + j;
+                const uint32_t top_right    = top_left + 1;
+                const uint32_t bottom_left  = (i + 1) * next + j;
+                const uint32_t bottom_right = bottom_left + 1;
+
+                sphere.faces.emplace_back(top_left, bottom_left, top_right);
+                sphere.faces.emplace_back(top_right, bottom_left, bottom_right);
+            }
+        }
+
+        return sphere;
     }
 }
--- code/core/primitives.hpp
+++ code/core/primitives.hpp
@@ -9,5 +9,7 @@ namespace editor
     public:
         static geometry create_plane(float x = 1.0f, float z = 1.0f);
         static geometry create_box(float   x = 1.0f, float y = 1.0f, float z = 1.0f);
+
+        static geometry create_sphere(uint32_t segments = 32, float radius = 1.0f);
     };
 }
