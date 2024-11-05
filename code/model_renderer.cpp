473a73ef883c1cd5db3ae454624014767f4645de
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-05 22:04:59 +0200

    Rename Model to ModelRenderer and update CMake configuration.
    The Model class has been renamed to ModelRenderer to better reflect its purpose. Associated files and namespaces have been updated accordingly. Additionally, CMake configurations have been adjusted to include the new model_renderer component and eliminate the outdated renderers directory.

--- code/model_renderer.cpp
+++ code/model_renderer.cpp
@@ -1,13 +1,13 @@
-#include "model.hpp"
+#include "model_renderer.hpp"
 
 #include "primitives/triangle.hpp"
 
 #include "opengl/commands.hpp"
 #include "opengl/macros.hpp"
 
-namespace editor::renderers
+namespace editor
 {
-    void Model::draw(const engine::opengl::VertexArray* vertex_array, const engine::mat4& model, const engine::rgb& color, const int32_t faces) const
+    void ModelRenderer::draw(const engine::opengl::VertexArray* vertex_array, const engine::mat4& model, const engine::rgb& color, const int32_t faces) const
     {
         _shader->push(0, model);
         _buffer->sub_data(engine::buffers::data::create(&color));
--- code/model_renderer.hpp
+++ code/model_renderer.hpp
@@ -6,9 +6,9 @@
 #include "mat4.hpp"
 #include "rgb.hpp"
 
-namespace editor::renderers
+namespace editor
 {
-    class Model final : public engine::opengl::Renderer
+    class ModelRenderer final : public engine::opengl::Renderer
     {
     public:
         void draw(const engine::opengl::VertexArray* vertex_array, const engine::mat4& model, const engine::rgb& color, int32_t faces) const;
