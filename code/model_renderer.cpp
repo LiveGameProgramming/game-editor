062f4b6b9cfa2a6ad1474a4cd80e3997dc888c40
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-10 14:47:07 +0200

    Refactor header includes and rename buffer method
    Rearranged header file inclusions for better clarity and organization. Renamed the _buffer->sub_data method to _buffer->update for improved readability and consistency.

--- code/model_renderer.cpp
+++ code/model_renderer.cpp
@@ -1,16 +1,14 @@
 #include "model_renderer.hpp"
+#include "opengl/commands.hpp"
 
 #include "primitives/triangle.hpp"
-
-#include "opengl/commands.hpp"
-#include "opengl/macros.hpp"
 
 namespace editor
 {
     void ModelRenderer::draw(const engine::opengl::VertexArray* vertex_array, const engine::mat4& model, const engine::rgb& color, const int32_t faces) const
     {
         _shader->push(model);
-        _buffer->sub_data(engine::buffers::data::create(&color));
+        _buffer->update(engine::buffers::data::create(&color));
 
         assert(vertex_array);
         vertex_array->bind();
