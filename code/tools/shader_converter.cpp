ff65d063f80b2079fbee51a82878706661edff89
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-14 19:54:18 +0200

    Add shader converter functionality
    Implemented a static `convert` method in `ShaderConverter` class that performs shader conversion using `glslangValidator`. Updated namespaces and CMakeLists to include new headers and precompile settings.

--- code/tools/shader_converter.cpp
+++ code/tools/shader_converter.cpp
@@ -2,4 +2,25 @@
 
 namespace editor::tools
 {
+    void ShaderConverter::convert(const std::filesystem::path& input, const std::filesystem::path& output)
+    {
+        std::vector<std::filesystem::path> files;
+
+        for (const auto& file : std::filesystem::directory_iterator(input.parent_path()))
+        {
+            if (file.is_regular_file() && file.path().filename().replace_extension() == input.filename())
+            {
+                files.push_back(file.path());
+            }
+        }
+
+        for (const auto& file : files)
+        {
+            const std::string filename = file.filename().string();
+
+            std::string command = std::format("glslangValidator -V -G -S {} -o {} {}", file.extension().string().erase(0, 1), output.string() + filename, input.parent_path().string() + "/" + file.filename().string());
+
+            std::system(command.c_str());
+        }
+    }
 }
--- code/tools/shader_converter.hpp
+++ code/tools/shader_converter.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
-namespace editor
+namespace editor::tools
 {
     class ShaderConverter
     {
+    public:
+        static void convert(const std::filesystem::path& input, const std::filesystem::path& output);
     };
 }
