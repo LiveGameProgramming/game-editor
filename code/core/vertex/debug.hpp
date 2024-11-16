fe3998cb81d777125d2ab768e7fab88cbdf626f9
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-16 21:45:09 +0200

    Refactor editor includes and debug namespace
    Updated CMakeLists to correct include and linking specifications. Refactored debug.hpp namespace and vector type to align with core conventions.

--- code/core/vertex/debug.hpp
+++ code/core/vertex/debug.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-namespace editor::core::vertex
+namespace core::vertex
 {
     struct debug
     {
-        engine::vec3 position;
-        engine::vec3 extra; // normal | color
+        math::vec3 position;
+        math::vec3 extra; // normal | color
     };
 }
